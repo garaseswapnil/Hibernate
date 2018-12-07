868a31bf96c04d480cf2a36f8dc5dda4b3a5c1f0
garaseswapnil <garaseswapnil@gmail.com>
2018-12-07 12:33:42 +0530

    first commit

---
+++ src/main/java/com/temp/TempDemo/App.java
@@ -0,0 +1,13 @@
+package com.temp.TempDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
